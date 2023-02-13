29c07400eeeec457e46f5c323c9a0377e5b3f18e
mandar <mandarbakal3@gmail.com>
2023-02-13 21:57:50 +0530

    Performed Operation

---
+++ ImathsFunc.java
@@ -0,0 +1,8 @@
+package com.bridgelabs;
+
+@FunctionalInterface
+public interface ImathsFunc
+{
+	int performedOperation(int a,int b);
+
+}
---
+++ Maths.java
@@ -0,0 +1,33 @@
+package com.bridgelabs;
+
+public class Maths
+{
+	public static void main(String[] args)
+	{
+		ImathsFunc imathsFunc1=(a,b)->(a+b);
+		System.out.println("Addition:"+imathsFunc1.performedOperation(12, 4));
+		
+		 ImathsFunc imathsFunc2=(a,b)->(a*b);
+			  System.out.println("Multiplication:"+imathsFunc2.performedOperation(12, 3));
+			  
+			  ImathsFunc imathsFunc3=(a,b)->(a/b);
+			  System.out.println("Division:"+imathsFunc3.performedOperation(12, 3));
+
+			  
+			  ImathsFunc imathsFunc4=(a,b)->(a*b);
+			  System.out.println("Substraction:"+imathsFunc4.performedOperation(12, 3));
+			 
+		
+		
+		  
+		 
+		
+	}	
+	
+		
+	
+	
+   
+	
+
+}
